ea7ab0f6fdbba29acdb453a350e017a8a75fa42c
Emiliano Menéndez <emystein@gmail.com>
2020-07-10 01:17:35 -0300

    Singular and Plural cardinality

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,16 +1,44 @@
 class Verse
     def self.number(number)
-        case number
-            when 0
-                ZeroBottleVerse.new
-            else
-                ManyBottlesVerse.new(number)
-        end
+        number == 0 ? ZeroBottleVerse.new : ManyBottlesVerse.new(number)
     end
 
     def to_s()
         "#{self.number_of_bottles().capitalize} of beer on the wall, #{self.number_of_bottles()} of beer.\n" \
         "#{self.take_bottle()}, #{self.next().number_of_bottles()} of beer on the wall.\n"
+    end
+end
+
+class ZeroBottleVerse  < Verse
+    def number_of_bottles()
+        'no more bottles'
+    end
+
+    def take_bottle()
+        'Go to the store and buy some more'
+    end
+
+    def next()
+        Verse.number(Bottles.total)
+    end
+end
+
+class ManyBottlesVerse < Verse
+    def initialize(number)
+        @number = number
+        @cardinality = number == 1 ? SingularCardinality.new : PluralCardinality.new
+    end
+
+    def number_of_bottles()
+        "#{@number} bottle#{@cardinality.suffix}"
+    end
+
+    def take_bottle()
+        "Take #{@cardinality.one} down and pass it around"
+    end
+
+    def next()
+        Verse.number(@number - 1)
     end
 end
 
@@ -34,50 +62,19 @@ class PluralCardinality
     end
 end
 
-class ManyBottlesVerse < Verse
-    def initialize(number)
-        @number = number
-        @cardinality = number == 1 ? SingularCardinality.new : PluralCardinality.new
-    end
-
-    def number_of_bottles()
-        "#{@number} bottle#{@cardinality.suffix}"
-    end
-
-    def take_bottle()
-        "Take #{@cardinality.one} down and pass it around"
-    end
-
-    def next()
-        if (@number > 1) then
-            ManyBottlesVerse.new(@number - 1)
-        else
-            ZeroBottleVerse.new()
-        end
-    end
-end
-
-class ZeroBottleVerse  < Verse
-    def number_of_bottles()
-        'no more bottles'
-    end
-
-    def take_bottle()
-        'Go to the store and buy some more'
-    end
-
-    def next()
-        ManyBottlesVerse.new(99)
-    end
-end
-
 class Bottles
-    def song()
-        self.verses(99, 0)
+    @@total = 99
+
+    def self.total
+        @@total
+    end
+
+    def song
+        self.verses(@@total, 0)
     end
 
     def verses(high, low)
-        high.downto(low).map { |bottle| verse(bottle).to_s }.join("\n")
+        high.downto(low).map { |bottle| self.verse(bottle) }.join("\n")
     end
 
     def verse(number)
